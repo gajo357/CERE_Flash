e8a32027e510e9fc0876db66f59a0f2ac95f4543
gajo357 <gajo357@gmail.com>
2016-08-17 08:56:10 +0200

    Working Newton

--- CERE Course/DAY6.for
+++ CERE Course/DAY6.for
@@ -18,20 +18,161 @@ C     P, T    PRESSURE, TEMPERATURE   (I)
       CHARACTER*4 COMPS(NCA)
       
       INTEGER IER
+      DOUBLE PRECISION X(NCA), Y(NCA)
       DOUBLE PRECISION TDEW, TBUB, PDEW, PBUB, BETA
             
       BETA = 0
-      CALL GETSATTEMP(NCA, P, Z, BETA, TBUB, IER)
+      CALL GETSATTEMP(NCA, P, Z, BETA, TBUB, X, Y, IER)
       CALL PRINTRSULT("BUBBLE POINT TEMPERATURE", P, TBUB, IER)
       CALL GETSATPRES(NCA, T, Z, BETA, PBUB, IER)
       CALL PRINTRSULT("BUBBLE POINT PRESSURE", PBUB, T, IER)
       
       BETA = 1
-      CALL GETSATTEMP(NCA, P, Z, BETA, TDEW, IER)
+      CALL GETSATTEMP(NCA, P, Z, BETA, TDEW, X, Y, IER)
       CALL PRINTRSULT("DEW POINT TEMPERATURE", P, TDEW, IER)
       CALL GETSATPRES(NCA, T, Z, BETA, PDEW, IER)
       CALL PRINTRSULT("DEW POINT PRESSURE", PDEW, T, IER)
       
+      CALL PHAENV(NCA, Z, IER)
+      
+      END SUBROUTINE
+
+      SUBROUTINE PHAENV(NCA, Z, IER)
+      IMPLICIT DOUBLE PRECISION (A-H, O,Z)
+C     VAR   VARIABLE TO GO BY
+      PARAMETER (NPOINTS=1000)
+      INTEGER NCA, VAR, IER
+      DOUBLE PRECISION Z(NCA)
+      DOUBLE PRECISION PHAT(NPOINTS), PHAP(NPOINTS)
+      
+      INTEGER IPOINT, INDX(NCA + 2), MAXITER
+      DOUBLE PRECISION P, T, BETA, SPEC
+      DOUBLE PRECISION KFACT(NCA), ZNORM(NCA)
+      DOUBLE PRECISION L(NCA), V(NCA)
+      DOUBLE PRECISION FUG(NCA), FUGL(NCA), FUGV(NCA)
+      DOUBLE PRECISION FUGT(NCA), FUGTL(NCA), FUGTV(NCA)
+      DOUBLE PRECISION FUGP(NCA), FUGPL(NCA), FUGPV(NCA)
+      DOUBLE PRECISION FUGX(NCA, NCA), FUGXL(NCA, NCA), FUGXV(NCA, NCA)
+      DOUBLE PRECISION JACOB(NCA + 2, NCA + 2), F(NCA + 2)
+      DOUBLE PRECISION X(NCA + 2), DX(NCA + 2)
+      DOUBLE PRECISION MAXERROR
+      
+C     NUMBER OF ITERATIONS FOR SAT T ALGORITHM
+      MAXITER = 3
+      MAXERROR = 1E-6
+      IER = 0
+      
+      SPEC = 0
+            
+      DO I=1,NCA
+          ZNORM(I) = Z(I) / SUM(Z)
+      ENDDO  
+      
+C     SMALL INITIAL PRESSURE AND START FROM THE BUBBLE POINT LINE
+      P = 0.3
+      BETA = 0
+      CALL GETSATTEMP(NCA, P, ZNORM, BETA, T, L, V, 
+     &                IER, MAXITER = MAXITER)
+      IPOINT = 1
+      PHAT(IPOINT) = T
+      PHAP(IPOINT) = P
+      DO I = 1,NCA
+          KFACT(I) = V(I)/L(I)
+          X(I) = LOG(KFACT(I))
+      ENDDO
+      X(NCA + 1) = LOG(T)
+      X(NCA + 2) = LOG(P)
+      DX(:) = 0
+      
+      WRITE(*,*)
+      WRITE(*,'(A, I4)') "CHOOSE VARIABLE TO GO BY FROM 1 TO ", NCA + 2
+      READ(*,*) VAR      
+                        
+      DO WHILE (.TRUE.)          
+          
+C         CHECK FUGACITY CEFFICIENTS
+          CALL THERMO(EXP(X(NCA + 1)), EXP(X(NCA + 2)), L, FUGL,
+     &     FUGT = FUGTL, FUGP = FUGPL, FUGX=FUGXL)
+C         CHECK FUGACITY CEFFICIENTS
+          CALL THERMO(EXP(X(NCA + 1)), EXP(X(NCA + 2)), V, FUGV,
+     &     FUGT = FUGTV, FUGP = FUGPV, FUGX=FUGXV)
+          
+C         CALCULATE JACOBIAN
+          DO I = 1,NCA+2
+              IF(I .LE. NCA) THEN
+                  F(I) = X(I) + FUGV(I) - FUGL(I)
+              ELSE IF(I .EQ. NCA + 1) THEN
+                  F(I) = 0
+                  DO J = 1,NCA
+                      F(I) = F(I) + (V(J) - L(J))
+                  ENDDO
+              ELSE 
+                  F(I) = 0
+                  SPEC = X(VAR)
+              ENDIF
+              
+              DO J = 1,NCA+2
+                  JACOB(I,J) = 0
+                  
+C                 DF(I)/DlnK(J)   I,J = 1...NCA
+                  IF(I .LE. NCA .AND. J .LE. NCA) THEN
+                      TEMP = 0
+                      IF (I .EQ. J) TEMP = 1.0
+                      JACOB(I,J) = TEMP + 
+     &                    FUGXV(I, J)*(1 - BETA)*L(J)*V(J)/ZNORM(J) +
+     &                    FUGXL(I, J)*BETA*L(J)*V(J)/ZNORM(J)
+C                 Df(I)/DlnP      I=1...NCA
+                  ELSE IF (I .LE. NCA .AND. J .EQ. NCA + 2) THEN
+                      JACOB(I,J) = (FUGPV(I) - FUGPL(I)) * EXP(X(J)) 
+C                 Df(I)/DlnT      I=1...NCA
+                  ELSE IF (I .LE. NCA .AND. J .EQ. NCA + 1) THEN
+                      JACOB(I,J) = (FUGTV(I) - FUGTL(I)) * EXP(X(J)) 
+C                 Df(NCA+1)/DlnK(J)
+                  ELSE IF (I .EQ. NCA + 1 .AND. J .LE. NCA) THEN
+                      JACOB(I,J) = L(J)*V(J)/ZNORM(J)
+C                 Df(NCA+1)/DlnP    Df(NCA + 1)/DlnT          
+                  ELSE IF (I .EQ. NCA + 1 .AND. J .GT. NCA) THEN
+                      JACOB(I,J) = 0
+C                 SPEC DERIVATIVE
+                  ELSE IF (I .EQ. NCA + 2) THEN
+                      IF(VAR .EQ. J) JACOB(I,J) = 1
+                  ENDIF                  
+              ENDDO
+          ENDDO
+          
+C         INVERT JACOBIAN AND GET DELTA X
+          CALL LUDEC(NCA + 2, INDX, JACOB)
+          CALL BACKSUBST(NCA + 2, INDX, JACOB, F)
+          
+C         DELTA X COMES IN THE VALUES OF F
+C         CALCULATE NEW X
+          DO I = 1,NCA + 2
+              DX(I) = F(I)
+              IF (DX(I) .NE. DX(I)) THEN
+                  IER = 601
+                  GOTO 101
+              END IF
+              
+              X(I) = X(I) - DX(I)
+          ENDDO
+          
+C         CHECK IF CONVERGED
+          DO I = 1,NCA + 2
+              IF (DX(I) .GE. MAXERROR) GOTO 100
+          ENDDO          
+          GOTO 200
+          
+100       CONTINUE
+C         GET NEW VAPOR LIQUID COMPOSITION
+          DO I = 1,NCA
+              KFACT(I) = EXP(X(I))
+          ENDDO
+          CALL GETCOMPOSITION(NCA, ZNORM, BETA, KFACT, L, V)
+      ENDDO
+      
+200   CONTINUE
+      
+101   CONTINUE
       END SUBROUTINE
 
       SUBROUTINE GETSATPRES(NCA, T, Z, BETA, PSAT, IER)
@@ -141,7 +282,7 @@ C         CHECK CONVERGENCE
       
       OPEN(13,FILE='PSATERROR.TXT',STATUS='NEW',BLANK='ZERO',ERR=101)
       GOTO 301
-      OPEN(13,FILE='PSATERROR.TXT',STATUS='OLD',BLANK='ZERO',ERR=101)      
+      OPEN(13,FILE='PSATERROR.TXT',STATUS='OLD',BLANK='ZERO',ERR=101)
 301   CONTINUE
       DO I = 1,ITER
           EP(I) = LOG(ABS (LOG(EP(I)) - LOG(PSAT)))
@@ -153,7 +294,7 @@ C         CHECK CONVERGENCE
 101   CONTINUE
       END SUBROUTINE
 
-      SUBROUTINE GETSATTEMP(NCA, P, Z, BETA, TSAT, IER)
+      SUBROUTINE GETSATTEMP(NCA, P, Z, BETA, TSAT, X, Y, IER, MAXITER)
 C     NCA      no. of components      (i)
 C     Z       feed mole fractions     (i)
 C     P                    PRESSURE   (I)
@@ -162,6 +303,7 @@ C     P                    PRESSURE   (I)
       INTEGER NCA
       DOUBLE PRECISION TSAT,P, BETA
       DOUBLE PRECISION Z(NCA), ZNORM(NCA)
+      INTEGER, OPTIONAL :: MAXITER
       
       INTEGER IER, ITER, CHECKCOMP
       DOUBLE PRECISION KFACT(NCA), X(NCA), Y(NCA)
@@ -241,6 +383,11 @@ C         CALCULATE TEMPERATURE STEP
           ENDIF          
           TSAT = TSAT + DT
           
+C         CHECK MAX NUMBER OF ITERATIONS
+          IF(PRESENT(MAXITER)) THEN
+              IF(ITER .EQ. MAXITER) GOTO 101
+          ENDIF
+                    
 C         CHECK CONVERGENCE
           IF(ABS(DT) .LT. MAXERROR) THEN
               DO I=1,NCA
